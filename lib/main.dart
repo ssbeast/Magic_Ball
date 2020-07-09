1514dbf4b19b3c6379b34d6f059ab5a81af0f208
ssbeast <ssbeast007@gmail.com>
2020-07-10 04:45:26 +0530

    base page made

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      MaterialApp(
-        home: null,
+      BallPage(),
+    );
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          backgroundColor: Colors.blue.shade900,
+          title: Text('Ask Me Anything'),
+        ),
+        body: Container(),
       ),
     );
+  }
+}
