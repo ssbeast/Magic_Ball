e76cdb091cf133047ff8cef15a7fb354242ed2d7
ssbeast <ssbeast007@gmail.com>
2020-07-10 05:12:28 +0530

    project completed

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,9 @@ class _BallState extends State<Ball> {
     return Center(
       child: FlatButton(
         onPressed: () {
-          ballNumber = Random().nextInt(5);
+          setState(() {
+            ballNumber = Random().nextInt(5) + 1;
+          });
         },
         child: Image.asset('images/ball$ballNumber.png'),
       ),
