f74d83116b6ea21526655db9f8b5a255d14b4f08
ssbeast <ssbeast007@gmail.com>
2020-07-10 04:50:39 +0530

    statefull made

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-      BallPage(),
+      MaterialApp(
+        home: BallPage(),
+      ),
     );
 
 class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.blue,
-        appBar: AppBar(
-          backgroundColor: Colors.blue.shade900,
-          title: Text('Ask Me Anything'),
-        ),
-        body: Container(),
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text('Ask Me Anything'),
       ),
+      body: Container(),
     );
+  }
+}
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
   }
 }
